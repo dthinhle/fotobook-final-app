59a5e99a16a157811d2467e8bb0824ebaf77c0f3
dthinhle <dthinhlee@gmail.com>
2019-06-25 17:18:52 +0700

    Add photo and album models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all
+Photo.delete_all
+Album.delete_all
+User.create(email: "admin@nus.com",password:'$2a$11$EJqNptSA0EGGhZZxXxsg5.xZYHMVHOyK2dMIu.P.3zIgRcSXUvuAK',first_name: 'admin',last_name: 'admin')
+
+1.upto(5){|i|
+  i = Photo.create([{title: "testPhoto#{i}"}])
+}
+
+1.upto(5){|i|
+  i = Album.create([{title: "testAlbum#{i}"}])
+}
+
+2.upto(5){|i|
+  User.create(email: "testUser#{i}@nus.com",password:'$2a$11$zD1QPtCOmLqsq0EAhHyg0.JP8MoXHbi36UNhERE/9BzhfnDbOUqaW',first_name: 'dummy',last_name: "user#{i}")
+}
+
