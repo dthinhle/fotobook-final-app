43ee470f569279001cde1570479a37d2cf36cb4a
dthinhle <dthinhlee@gmail.com>
2019-06-26 12:04:18 +0700

    Refine all the import modules.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'welcome/signin'
+  get 'feed/home'
   root 'welcome#signin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
