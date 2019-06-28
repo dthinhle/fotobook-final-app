8a03d6cf973c1d17b70dedca54aa76d6732bd67d
dthinhle <dthinhlee@gmail.com>
2019-06-28 15:22:51 +0700

    Add photo and album validations.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,7 @@
 class Photo < ApplicationRecord
   belongs_to :imageable, polymorphic: true, optional: true
+
+  validates :title, presence: true
+  validates :desc, length: {maximum: 500}
+  validates :imageable_type, exclusion: {in: %w"user album", message: "%{value} is invalid."}
 end
