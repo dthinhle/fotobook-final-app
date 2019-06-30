8ee4678b66313eb1caf29bbe031ca70e1f494dbf
dthinhle <dthinhlee@gmail.com>
2019-06-30 15:53:30 +0700

    Add myprofile view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'welcome/signin'
-  get 'feed/home'
+  get 'myprofile', to: 'profiles#myprofile', as: 'myprofile'
+  get 'home', to: 'feed#home', as: 'home'
   root 'welcome#signin'
+  root to: 'feed#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
