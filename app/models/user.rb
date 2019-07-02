5f3fef7a09598159f2e2edb28a40c0ac2ee2419a
dthinhle <dthinhlee@gmail.com>
2019-07-02 13:35:56 +0700

    Count photos and albums from database.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :photos, as: :imageable
+  has_many :albums
 
   validates :first_name, :last_name, presence: true, length: { maximum: 25, too_long: "25 characters are maximum allowed!" }
   validates :email, length: { maximum: 255, too_long: "255 characters are maximum allowed!" }
