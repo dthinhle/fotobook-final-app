aa6a466eea7d82dc7b9aa409304c9651552f04ff
dthinhle <dthinhlee@gmail.com>
2019-07-02 17:15:29 +0700

    Add edit profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,24 @@
 class ProfilesController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+    p "=========#{profile_params}"
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    puts current_user.id
+    puts @user.id
+    unless current_user.id == @user.id
+      redirect_to profile_path(@user)
+      flash[:notice] = "You can just edit other people profile like this, right?"
+    end
+  end
+
+  private
+  def profile_params
+    params.require(:user).permit(:id,:first_name,:last_name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
+  get 'profiles/myprofile'
   devise_for :users
-
-  get 'welcome/signin'
-  get 'myprofile', to: 'profiles#myprofile', as: 'myprofile'
   get 'home', to: 'feed#home', as: 'home'
   get 'discover', to: 'feed#discover', as: 'discover'
-
+  get 'myprofile', to: 'profiles#myprofile'
+  resources 'profiles', only: ['show', 'update', 'edit']
 
   root 'feed#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
